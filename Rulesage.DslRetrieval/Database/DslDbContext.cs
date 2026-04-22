7c94b748e5b4318308dcfa5c46736edd891bff9d
Chshine1 <chshine@qq.com>
2026-04-23 00:55:23 +0800

    feat: Create dsl entry and implement an embedding service

---
+++ Rulesage.DslRetrieval/Database/DslDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rulesage.DslRetrieval.Database.Entities;
+
+namespace Rulesage.DslRetrieval.Database;
+
+public class DslDbContext(DbContextOptions<DslDbContext> options) : DbContext(options)
+{
+    public DbSet<DslEntry> DslEntries => Set<DslEntry>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasPostgresExtension("vector");
+
+        modelBuilder.Entity<DslEntry>(entity =>
+        {
+            entity.HasIndex(e => e.Embedding)
+                .HasMethod("ivfflat")
+                .HasOperators("vector_cosine_ops");
+        });
+    }
+}
---
+++ Rulesage.DslRetrieval/Embedding/Abstractions/IEmbeddingService.cs
@@ -0,0 +1,7 @@
+﻿namespace Rulesage.DslRetrieval.Embedding.Abstractions;
+
+public interface IEmbeddingService
+{
+    float[] GetEmbedding(string text, int chunkSize = 200, int overlapSize = 50);
+    public float[][] GetBatchEmbeddings(IReadOnlyList<long[]> tokenizedTexts);
+}
